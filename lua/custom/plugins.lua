589356e3daded6701035b86f8f7198e1256834a0
mrbya <v.toth.svk2@gmail.com>
2024-03-12 18:03:29 +0100

    base nvim config

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,16 @@
+local plugins = {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = { "c", "lua", "vim", "angular", "arduino", "asm", "awk", "bash", "c_sharp", "cmake", "comment", "cpp", "css", "csv", "diff", "disassembly", "dockerfile", "doxygen", "fortran", "fsh", "gdscript", "git_config", "git_rebase", "gitattributes", "gitcommit", "hjson", "html", "http", "ini", "java", "javascript", "jq", "json", "json5", "kconfig", "linkerscript", "llvm", "luadoc", "make", "markdown", "markdown_inline", "matlab", "meson", "ninja", "objdump", "pascal", "passwd", "pem", "perl", "php", "phpdoc", "pod", "poe_filter", "pymanifest", "python", "regex", "requirements", "sql", "ssh_config", "tcl", "tmux", "toml", "typescript", "v", "verilog", "vim", "vimdoc", "vue", "xml", "yaml"},
+
+          highlight = {
+            enable = true,
+            use_languagetree = true,
+          },
+
+          indent = { enable = true },
+        },
+    },
+}
+return plugins
