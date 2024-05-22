fb527b2c71db0e117c16f61d1cf47a6a0f0b6b92
mrbya <v.toth.svk2@gmail.com>
2024-05-22 10:26:35 +0200

    added custom config for perl ls

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,32 @@
+local plugins = {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = { "c", "lua", "vim", "angular", "arduino", "asm", "awk", "bash", "c_sharp", "cmake", "comment", "cpp", "css", "csv", "diff", "disassembly", "dockerfile", "doxygen", "fortran", "fsh", "gdscript", "git_config", "git_rebase", "gitattributes", "gitcommit", "hjson", "html", "http", "ini", "java", "javascript", "jq", "json", "json5", "kconfig", "linkerscript", "llvm", "luadoc", "make", "markdown", "markdown_inline", "matlab", "meson", "ninja", "objdump", "pascal", "passwd", "pem", "perl", "php", "phpdoc", "pod", "poe_filter", "pymanifest", "python", "regex", "requirements", "sql", "ssh_config", "tcl", "tmux", "toml", "typescript", "v", "verilog", "vim", "vimdoc", "vue", "xml", "yaml", "devicetree"},
+
+          highlight = {
+            enable = true,
+            use_languagetree = true,
+          },
+
+          indent = { enable = true },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "perlnavigator",
+            },
+        },
+    },
+}
+
+return plugins
