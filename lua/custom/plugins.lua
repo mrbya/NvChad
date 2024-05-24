09427a6860abd9543c42d016828edf4d3b28c835
mrbya <v.toth.svk2@gmail.com>
2024-05-24 17:33:50 +0200

    added keymap loader

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -12,5 +12,36 @@ local plugins = {
           indent = { enable = true },
         },
     },
+    {
+        'SuperBo/fugit2.nvim',
+        opts = {
+            width = 70,
+        },
+            dependencies = {
+            'MunifTanjim/nui.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'nvim-lua/plenary.nvim',
+            {
+                'chrisgrieser/nvim-tinygit', -- optional: for Github PR view
+                dependencies = { 'stevearc/dressing.nvim' }
+            },
+        },
+        cmd = {
+            'Fugit2',
+            'Fugit2Diff',
+            'Fugit2Graph'
+        },
+        lazy = false,
+    },
+    {
+        dir = '.',
+        name = 'keyloader',
+        config = function ()
+            for section, x in pairs(require('custom.mappings.mappings')) do
+                require('core.utils').load_mappings(section)
+            end
+        end,
+        lazy = false
+    },
 }
 return plugins
