e087b1a2338d164962122c1ad36c87f4595b06d0
Toth, Viktor (SMO RI ML TC MIREL ADC PSD) <viktor.toth@siemes.com>
2025-05-14 11:42:24 +0200

    Added python config

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,7 +4,20 @@ local capabilities = base.capabilities
 
 local lspconfig = require('lspconfig')
 
+local servers = {
+  "pyright",
+  "ruff_lsp",
+}
+
 lspconfig.bashls.setup{
     on_attach = on_attach,
     capabilities = capabilities
 }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {"python"},
+  })
+end
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -13,6 +13,54 @@ local plugins = {
         },
     },
     {
+        "nvim-neotest/nvim-nio",
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup()
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+        end
+    },
+    {
+        "mfussenegger/nvim-dap",
+        config = function(_, opts)
+            require("core.utils").load_mappings("dap")
+        end
+    },
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+        },
+        config = function(_, opts)
+            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+            require("dap-python").setup(path)
+            require("core.utils").load_mappings("dap_python")
+        end,
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        ft = {"python"},
+        opts = function()
+            return require "custom.configs.null-ls"
+        end,
+    },
+    {
         "neovim/nvim-lspconfig",
         config = function ()
             require 'plugins.configs.lspconfig'
@@ -44,7 +92,7 @@ local plugins = {
         dir = '.',
         name = 'keyloader',
         config = function ()
-            for section, x in pairs(require('custom.mappings.mappings')) do
+            for section, _ in pairs(require('custom.mappings.mappings')) do
                 require('core.utils').load_mappings(section)
             end
         end,
@@ -56,6 +104,11 @@ local plugins = {
             ensure_installed = {
                 'bash-language-server',
                 'lua-language-server',
+                'pyright',
+                'ruff-lsp',
+                'black',
+                'debugpy',
+                'mypy',
             }
         }
     },
